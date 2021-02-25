620592b52326e7be1b0cf780f1e8c7a97ba5331d
MarcwithaC1 <68074266+MarcwithaC1@users.noreply.github.com>
2021-02-25 10:17:26 -0500

    Add files via upload

---
+++ precache-manifest.781d22f89447ee6d5d0cbc73e797c653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9d7c5e5835945f7085e5c4baf136b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a1ef8b5c7be6ea8539c",
+    "url": "/static/css/main.ee6a2425.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a1ef8b5c7be6ea8539c",
+    "url": "/static/js/main.fbef70ab.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
---
+++ src/SingleColor.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+const SingleColor = ({ rgb, weight, index, hexColor, type }) => {
+  console.log(type);
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+      className={`color ${type === "shade" && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
